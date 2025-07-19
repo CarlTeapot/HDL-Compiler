382e97942370273b886bc01a66e5f9e4c8fb72d0
CarlTeapot <arevadzemate@gmail.com>
2025-07-20 00:47:51 +0200

    Main: implement built-in chips and chip skeleton

--- models/built_ins/built_in_chips.py
+++ models/built_ins/built_in_chips.py
@@ -1,10 +1,25 @@
-def nand_gate(a: bool, b: bool) -> bool:
+
+
+def _nand_gate(a: bool, b: bool) -> bool:
     return not (a and b)
 
-def not_gate(a: bool, b: bool) -> bool:
+def _not_gate(a: bool, b: bool) -> bool:
     return not (a and b)
-def or_gate(a: bool, b: bool) -> bool:
+
+def _or_gate(a: bool, b: bool) -> bool:
     return a or b
 
-def and_gate(a: bool, b: bool) -> bool:
+def _and_gate(a: bool, b: bool) -> bool:
     return a and b
+
+def built_in_chip(chip_name: str, a: bool, b: bool) -> bool :
+    if chip_name == 'nand':
+        return _nand_gate(a,b)
+    elif chip_name == 'not':
+        return _not_gate(a,b)
+    elif chip_name == 'or':
+        return _or_gate(a,b)
+    elif chip_name == 'and':
+        return _and_gate(a,b)
+
+    return False;
